48cc109457723ae4e085d363b3923bb08ec74ef5
Szymon Bacanski <s.bacanski@gmail.com>
2022-07-07 20:29:30 +0200

    New Navbar added
    New Navbar added.
    Added TODO about Button component.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { StyledNavbar } from './Navbar.style.js';
 import icon from '../../assets/beer-outline-white.png';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+	const [isMobileMenuActive, setIsMobileMenuActive] = useState(false);
+
+	const handleMobileMenuIcon = () => setIsMobileMenuActive(!isMobileMenuActive);
+	const closeMobileMenu = () => setIsMobileMenuActive(false);
+
 	return (
 		<>
 			<StyledNavbar>
 				<div className='wrapper'>
 					<div className='icon'>
-						<Link to='/'>
+						<Link to='/' onClick={closeMobileMenu}>
 							<img src={icon} alt='Main app icon - beer glass' />
 						</Link>
 					</div>
-					<div className='navbar-container'>
+					<div className='mobile-menu-container'>
+						{isMobileMenuActive ? (
+							<FaTimes
+								id='mobile-menu-icon'
+								onClick={handleMobileMenuIcon}
+							/>
+						) : (
+							<FaBars
+								id='mobile-menu-icon'
+								onClick={handleMobileMenuIcon}
+							/>
+						)}
+					</div>
+					<div
+						className={
+							isMobileMenuActive
+								? 'navbar-container active'
+								: 'navbar-container '
+						}
+					>
 						<ul>
-							<li className='navbar-element'>
+							<li className='navbar-element' onClick={closeMobileMenu}>
 								<Link to='/' className='link'>
 									<span>Home</span>
 								</Link>
 							</li>
-							<li className='navbar-element'>
+							<li className='navbar-element' onClick={closeMobileMenu}>
 								<Link to='/' className='link'>
 									<span>Products</span>
 								</Link>
 							</li>
-							<li className='navbar-element'>
+							<li className='navbar-element' onClick={closeMobileMenu}>
 								<Link to='/' className='link'>
 									<span>Add</span>
 								</Link>
 							</li>
-							<li className='navbar-element'>
+							<li className='navbar-element' onClick={closeMobileMenu}>
 								<Link to='/' className='link'>
 									<span>Order List</span>
 								</Link>
 							</li>
+							<li
+								className='navbar-element'
+								id='signup-button'
+								onClick={closeMobileMenu}
+							>
+								<Link to='/'>
+									<button>Sign up</button>
+									{/* TODO: Create BUTTON Component */}
+								</Link>
+							</li>
 						</ul>
 					</div>
-					{/* <div className='login-container'>
-						<Button content={'sign up'} isRounded={false} />
-					</div> */}
-					<button>SignUp</button>
+					<div className='logout-container'>
+						<Link to='/' id='logout-button'>
+							<button>Logout</button>
+						</Link>
+					</div>
 				</div>
 			</StyledNavbar>
 		</>
--- src/components/Navbar/Navbar.style.js
+++ src/components/Navbar/Navbar.style.js
@@ -23,17 +23,20 @@ export const StyledNavbar = styled.div`
 		justify-content: space-between;
 		padding: 1.5rem 7%;
 
-		/* max-width: 80%; */
+		/* max-width: 90%; */
 	}
 
 	.icon {
 		max-width: 4rem;
-		/* border: 1px dashed yellow; */
 
 		img {
 			width: 100%;
 			user-select: none;
 		}
+	}
+
+	.mobile-menu-container {
+		display: none;
 	}
 
 	.navbar-container {
@@ -48,6 +51,10 @@ export const StyledNavbar = styled.div`
 			padding: 0.5rem 0.1rem;
 		}
 
+		#signup-button {
+			display: none;
+		}
+
 		.link {
 			text-decoration: none;
 		}
@@ -60,6 +67,106 @@ export const StyledNavbar = styled.div`
 		.navbar-element span {
 			color: var(--main-color);
 			font-size: 1.2rem;
+		}
+	}
+
+	.login-container {
+		#signup-button {
+		}
+	}
+
+	@media screen and (max-width: 960px) {
+		.wrapper {
+			/* border: 1px solid var(--main-color); */
+		}
+		.icon {
+			img {
+			}
+		}
+
+		.mobile-menu-container {
+			display: flex;
+			#mobile-menu-icon {
+				color: #fff;
+				font-size: 2.2rem;
+			}
+		}
+
+		.mobile-menu-container:hover {
+			display: flex;
+
+			cursor: pointer;
+		}
+
+		.navbar-container {
+			/* border: 1px solid white; */
+
+			position: absolute;
+			top: 6rem;
+			left: -100%;
+			width: 100%;
+			height: calc(100vh - 6rem);
+			transition: all 0.5s ease;
+		}
+		.navbar-container.active {
+			left: 0;
+			background-color: var(--bg);
+			transition: all 0.6s ease;
+			z-index: 1;
+		}
+
+		.navbar-container {
+			ul {
+				/* border: 1px dashed var(--main-color); */
+
+				width: 100%;
+				height: 100%;
+				flex-direction: column;
+				align-items: center;
+				justify-content: flex-start;
+			}
+
+			.navbar-element {
+				/* border: 1px dashed var(--main-color); */
+
+				width: 100%;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				flex: 1;
+				margin: 0;
+				max-height: 4rem;
+			}
+
+			.link {
+				/* border: 1px dashed white; */
+			}
+
+			.navbar-element:hover {
+				transition: all 0.3s ease;
+				cursor: pointer;
+				background-color: var(--main-color);
+				span {
+					transition: all 0.1s ease;
+					font-size: 1.5rem;
+					color: var(--bg);
+				}
+			}
+
+			.navbar-element span {
+			}
+
+			.navbar-element#signup-button {
+				display: flex;
+			}
+		}
+
+		.logout-container {
+			border: 1px solid white;
+			height: 100%;
+			display: none;
+			#signup-button {
+			}
 		}
 	}
 `;
