deefc081adcddcb5616dd3d87dc2af8f36924932
Szymon Bacanski <s.bacanski@gmail.com>
2022-07-26 22:18:18 +0200

    Work still in progress
    <select> tag and problem with its value

--- src/Pages/AddProductPage/AddProductPage.jsx
+++ src/Pages/AddProductPage/AddProductPage.jsx
@@ -1,35 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyledAddProductPage } from './AddProductPage.style';
+import Button from '../../components/Button/Button';
 
 const AddProductPage = () => {
+	const [name, setName] = useState('');
+	const [type, setType] = useState('');
+	const [expDate, setExpDate] = useState('');
+	const [description, setDescription] = useState('');
+	const [photoLink, setPhotoLink] = useState('');
+
+	const handleClick = e => {
+		console.log(type);
+	};
+
 	return (
 		<StyledAddProductPage>
 			<div className='wrapper'>
-				<h1 className='heading'>Add Product</h1>
-				<div className='container'>
-					<form>
-						<div className='name-input'>
-							<label htmlFor=''>name</label>
-							<input type='text' />
-						</div>
-						<div className='type-input'>
-							<label htmlFor=''>type</label>
-							<input type='text' />
-						</div>
-						<div className='exp-date-input'>
-							<label>expiration date</label>
-							<input type='text' />
-						</div>
-						<div className='des-input'>
-							<label>description</label>
-							<input type='text' />
-						</div>
-						<div className='photo-link-input'>
-							<label htmlFor=''></label>
-							<input type='text' />
-						</div>
-					</form>
-				</div>
+				<h1 id='page-heading'>
+					Add <span>Product</span>
+				</h1>
+
+				<form>
+					{/* TODO: poprawić i przemieścić 'input' od wybierania typów; przestylować */}
+					{/* TODO: sprawdzic jak wyciagnac dane z wybranego elementu z tagu select */}
+					<div className='inputBox'>
+						<input type='text' placeholder='name' />
+					</div>
+					<div className='inputBox'>
+						<select
+							placeholder='choose value'
+							value={type}
+							onChange={e => setType(e.target.value)}
+						>
+							<option value='malt'>malt</option>
+							<option value='hop'>hop</option>
+							<option value='yeast'>yeast</option>
+							<option value='adjunct'>adjunct</option>
+							<option value='malt extracts'>malt extracts</option>
+						</select>
+					</div>
+
+					<div className='inputBox'>
+						<input type='text' placeholder='expiration date' />
+					</div>
+					<div className='inputBox'>
+						<input type='text' placeholder='description' />
+					</div>
+					<div className='inputBox'>
+						{/* TODO: przerobic komponent button tak, aby animacja :hover była osobnym atrybutem - default = none animation */}
+						{/* <Button btn_style='btn-default' btn_size='btn-normal'>
+							Add
+						</Button> */}
+					</div>
+					{/* <Button btn_style='btn-default' btn_size='btn-wide'>
+						Add
+					</Button> */}
+					<button onClick={handleClick}>Dodaj</button>
+				</form>
 			</div>
 		</StyledAddProductPage>
 	);
--- src/Pages/AddProductPage/AddProductPage.style.js
+++ src/Pages/AddProductPage/AddProductPage.style.js
@@ -1,3 +1,55 @@
 import styled from 'styled-components';
 
-export const StyledAddProductPage = styled.div``;
+export const StyledAddProductPage = styled.div`
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		/* justify-content: center; */
+		/* align-items: center; */
+
+		padding: var(--main-padding);
+	}
+
+	form {
+		background-color: var(--black);
+		/* text-align: center; */
+		padding: 5rem 2rem;
+		display: flex;
+		flex-direction: column;
+
+		.inputBox {
+			/* border: 1px dotted yellow; */
+
+			/* max-width: 45rem; */
+			margin-top: 1rem;
+			margin-bottom: 1rem;
+			background-color: var(--bg);
+
+			input,
+			select {
+				width: 100%;
+				padding: 0.8rem;
+				font-size: 1.1rem;
+				background: none;
+				text-transform: capitalize;
+			}
+
+			input {
+				color: #000;
+
+				::placeholder {
+					color: #fff;
+					opacity: 1;
+				}
+			}
+
+			select {
+				color: #fff;
+
+				option {
+					background-color: var(--bg);
+				}
+			}
+		}
+	}
+`;
